880d0d8006c5d8502483112a0adbe032626efd08
codeHarvest <madhusudanbhardwaj94@gmail.com>
2024-12-10 23:27:04 +0530

    Add test for summation of two numbers

--- index.spec.js
+++ index.spec.js
@@ -8,4 +8,8 @@ describe("String Calculator: add function", () => {
   test("returns the number itself if only one number is given", () => {
     expect(add("1")).toBe(1);
   });
+
+  test("returns the sum of two comma-separated numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
 });
